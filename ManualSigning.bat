6fcd7260851839a458eb21acbc8721029edd36ec
Andrey Rodin <andrey.val.rodin@gmail.com>
2026-02-04 10:18:02 +0300

    Added ManualSigning/bat

---
+++ ManualSigning.bat
@@ -0,0 +1,2 @@
+jarsigner -verbose -sigalg SHA256withRSA -digestalg SHA-256 -keystore "C:\Users\andre\AppData\Local\Xamarin\Mono for Android\Keystore\andrey-rodin\andrey-rodin.keystore" "C:\Users\andre\OneDrive\Desktop\com.companyname.myflat.maui.apk" andrey-rodin
+pause
